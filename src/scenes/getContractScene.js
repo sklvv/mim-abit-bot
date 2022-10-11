b7b56c11d5625b050364b9ffeebe647e9b3c5217
sklvv <iisokolov1@gmail.com>
2022-10-11 22:19:57 +0500

    gds

--- src/scenes/getContractScene.js
+++ src/scenes/getContractScene.js
@@ -4,4 +4,6 @@ import VkBotMarkup from "node-vk-bot-api/lib/markup.js";
 export const getContractScene = new VkBotScene("getContract", async (ctx) => {
   ctx.scene.next();
   await ctx.reply("get docs scene");
+
+  //2134fas
 });

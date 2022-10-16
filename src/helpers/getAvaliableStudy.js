ec921965d6cd28a877a6c29ddb0ac0f289c4866c
sklvv <iisokolov1@gmail.com>
2022-10-16 21:09:21 +0500

    chore: delete tests

--- src/helpers/getAvaliableStudy.js
+++ src/helpers/getAvaliableStudy.js
@@ -13,21 +13,3 @@ export const getAvaliableStudy = (examsArr) => {
   });
   return result;
 };
-
-const abit1 = [
-  { subjectName: "Русский Язык", points: "100" },
-  { subjectName: "Математика (Профиль)", points: "68" },
-  { subjectName: "История", points: "79" },
-];
-
-const abit2 = [
-  { subjectName: "Иностранный язык", points: "100" },
-  { subjectName: "Русский Язык", points: "68" },
-  { subjectName: "Обществознание", points: "79" },
-];
-
-const abit3 = [
-  { subjectName: "Русский Язык", points: "100" },
-  { subjectName: "Математика (Профиль)", points: "68" },
-  { subjectName: "История", points: "79" },
-];
